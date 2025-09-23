b3dd41726c8780ce1d0bdcec772d9ab2db3c3ab2
Dario <dariomartin3@gmail.com>
2025-09-23 15:29:25 -0300

    feat: add database constraints and checks

--- script/script_ddl_proyecto.sql
+++ script/script_ddl_proyecto.sql
@@ -1,4 +1,15 @@
 ------------------------------
+-- CREACION DE BASE DE DATOS
+------------------------------
+CREATE DATABASE gimnasio_db;
+GO
+------------------------------
+-- USO DE LA BASE DE DATOS
+------------------------------
+USE gimnasio_db;
+GO
+
+------------------------------
 -- TABLA ROL
 ------------------------------
 CREATE TABLE rol (
@@ -8,7 +19,6 @@ CREATE TABLE rol (
   --CLAVES PRIMARIAS
   CONSTRAINT PK_rol PRIMARY KEY (id_rol)
 )
-GO
 
 ------------------------------
 -- TABLA PERSONA
@@ -39,7 +49,6 @@ CREATE TABLE persona (
   CONSTRAINT DF_fecha_alta DEFAULT GETDATE(),
   CONSTRAINT DF_estado DEFAULT 1
 )
-GO
 
 ------------------------------
 -- TABLA USUARIO
@@ -60,7 +69,6 @@ CREATE TABLE usuario (
   --RESTRICCIONES UNIQUE
   CONSTRAINT UQ_username UNIQUE (username)
 )
-GO
 
 ------------------------------
 -- TABLA SOCIO
@@ -76,7 +84,6 @@ CREATE TABLE socio (
   --CLAVES FORANEAS
   CONSTRAINT FK_socio_persona FOREIGN KEY (id_socio) REFERENCES persona(id_persona)
 )
-GO
 
 ------------------------------
 -- TABLA MEMBRESIA_TIPO
@@ -94,7 +101,7 @@ CREATE TABLE membresia_tipo (
   CONSTRAINT CK_duracion_dias CHECK (duracion_dias > 0),
   CONSTRAINT CK_precio CHECK (precio > 0)
 )
-GO
+
 
 CREATE TABLE [membresia] (
   [id_membresia] int PRIMARY KEY IDENTITY(1, 1),
@@ -105,7 +112,7 @@ CREATE TABLE [membresia] (
   [fecha_fin] date,
   [estado] boolean
 )
-GO
+
 
 CREATE TABLE [pago] (
   [id_pago] int PRIMARY KEY IDENTITY(1, 1),
@@ -114,13 +121,13 @@ CREATE TABLE [pago] (
   [monto] decimal,
   [medio_pago] nvarchar(255)
 )
-GO
+
 
 CREATE TABLE [actividad] (
   [id_actividad] int PRIMARY KEY IDENTITY(1, 1),
   [nombre] nvarchar(255)
 )
-GO
+
 
 CREATE TABLE [clase] (
   [id_clase] int PRIMARY KEY IDENTITY(1, 1),
@@ -128,46 +135,90 @@ CREATE TABLE [clase] (
   [usuario_id] int,
   [cupo] int
 )
-GO
 
-CREATE TABLE [clase_dia] (
-  [clase_id] int,
-  [dias_id] int
+------------------------------
+-- RELACION ENTRE CLASE Y DIA
+------------------------------
+CREATE TABLE clase_dia (
+  clase_id int,
+  dias_id int,
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_clase_dia PRIMARY KEY (clase_id, dias_id),
 )
-GO
 
-CREATE TABLE [proveedor] (
-  [id_proveedor] int PRIMARY KEY IDENTITY(1, 1),
-  [nombre] nvarchar(255),
-  [cuit] int,
-  [email] nvarchar(255),
-  [telefono] int
+------------------------------
+-- TABLA PROVEEDOR
+------------------------------
+CREATE TABLE proveedor (
+  id_proveedor int IDENTITY(1, 1),
+  nombre varchar(150) NOT NULL,
+  cuit int NOT NULL,
+  email varchar(200) NOT NULL,
+  telefono int NOT NULL,
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_proveedor PRIMARY KEY (id_proveedor),
+  --RESTRICCIONES UNIQUE
+  CONSTRAINT UQ_cuit UNIQUE (cuit),
+  CONSTRAINT UQ_email UNIQUE (email),
+  CONSTRAINT UQ_telefono UNIQUE (telefono)
 )
-GO
 
-CREATE TABLE [orden_compra] (
-  [id_orden] int PRIMARY KEY IDENTITY(1, 1),
-  [proveedor_id] int,
-  [usuario_id] int,
-  [fecha] date,
-  [estado] nvarchar(255)
+------------------------------
+-- TABLA ESTADO DE LA COMPRA
+------------------------------
+CREATE TABLE estado_compra (
+  id_estado int IDENTITY(1, 1),
+  nombre varchar(100) NOT NULL, -- PENDIENTE, APROBADA, RECIBIDA, CANCELADA
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_estado_compra PRIMARY KEY (id_estado)
+) 
+  
+------------------------------
+-- TABLA CABECERA DEL ORDEN DE COMPRA
+------------------------------
+CREATE TABLE orden_compra (
+  id_orden int IDENTITY(1, 1),
+  proveedor_id int,
+  usuario_id int,
+  estado_id int,
+  fecha date DEFAULT GETDATE(),  
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_orden_compra PRIMARY KEY (id_orden),
+  --CLAVES FORANEAS
+  CONSTRAINT FK_orden_compra_proveedor FOREIGN KEY (proveedor_id) REFERENCES proveedor(id_proveedor),
+  CONSTRAINT FK_orden_compra_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario),
+  CONSTRAINT FK_orden_compra_estado FOREIGN KEY (estado_id) REFERENCES estado_compra(id_estado)  
 )
-GO
 
-CREATE TABLE [orden_compra_item] (
-  [id_item] int PRIMARY KEY IDENTITY(1, 1),
-  [orden_id] int,
-  [item_tipo] int,
-  [cantidad] int,
-  [precio_compra] decimal(10,2)
+------------------------------
+-- TABLA DETALLE DEL ORDEN DE COMPRA
+------------------------------
+CREATE TABLE orden_compra_item (
+  id_item int IDENTITY(1, 1),
+  orden_id int,
+  inventario_id int,
+  cantidad int NOT NULL,
+  precio_compra decimal(10,2) NOT NULL,
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_orden_compra_item PRIMARY KEY (id_item),
+  --CLAVES FORANEAS
+  CONSTRAINT FK_orden_compra_item_orden_compra FOREIGN KEY (orden_id) REFERENCES orden_compra(id_orden),
+  CONSTRAINT FK_orden_compra_item_inventario FOREIGN KEY (inventario_id) REFERENCES inventario(id_inventario),
+  --RESTRICCIONES CHECK
+  CONSTRAINT CK_cantidad CHECK (cantidad > 0),
+  CONSTRAINT CK_precio_compra CHECK (precio_compra > 0)  
 )
-GO
 
-CREATE TABLE [inventario_categoria] (
-  [id_categoria] int PRIMARY KEY IDENTITY(1, 1),
-  [nombre] nvarchar(255)
+------------------------------
+-- TABLA CATEGORIA DEL INVENTARIO
+------------------------------
+CREATE TABLE inventario_categoria (
+  id_categoria int IDENTITY(1, 1),
+  nombre varchar(200) NOT NULL,
+  --CLAVES PRIMARIAS
+  CONSTRAINT PK_inventario_categoria PRIMARY KEY (id_categoria)
 )
-GO
+
 
 CREATE TABLE [inventario] (
   [id_inventario] int PRIMARY KEY IDENTITY(1, 1),
@@ -177,7 +228,7 @@ CREATE TABLE [inventario] (
   [fecha_ingreso] date,
   [estado] boolean
 )
-GO
+
 
 CREATE TABLE [dias] (
   [id_dias] int PRIMARY KEY IDENTITY(1, 1),
@@ -185,7 +236,7 @@ CREATE TABLE [dias] (
   [hora_desde] time,
   [hora_hasta] time
 )
-GO
+
 
 CREATE TABLE [inventario_movimiento] (
   [id_movimiento] int PRIMARY KEY IDENTITY(1, 1),
@@ -195,71 +246,11 @@ CREATE TABLE [inventario_movimiento] (
   [cantidad] int,
   [compra_id] int
 )
-GO
+
 
 CREATE TABLE [clase_inscripcion] (
   [id_inscripcion] int PRIMARY KEY IDENTITY(1, 1),
   [socio_id] int,
   [clase_id] int
 )
-GO
 
-ALTER TABLE [usuario] ADD FOREIGN KEY ([id_usuario]) REFERENCES [persona] ([id_persona])
-GO
-
-ALTER TABLE [socio] ADD FOREIGN KEY ([id_socio]) REFERENCES [persona] ([id_persona])
-GO
-
-ALTER TABLE [usuario] ADD FOREIGN KEY ([rol_id]) REFERENCES [rol] ([id_rol])
-GO
-
-ALTER TABLE [membresia] ADD FOREIGN KEY ([socio_id]) REFERENCES [socio] ([id_socio])
-GO
-
-ALTER TABLE [membresia] ADD FOREIGN KEY ([tipo_id]) REFERENCES [membresia_tipo] ([id_tipo])
-GO
-
-ALTER TABLE [pago] ADD FOREIGN KEY ([membresia_id]) REFERENCES [membresia] ([id_membresia])
-GO
-
-ALTER TABLE [clase] ADD FOREIGN KEY ([actividad_id]) REFERENCES [actividad] ([id_actividad])
-GO
-
-ALTER TABLE [orden_compra] ADD FOREIGN KEY ([proveedor_id]) REFERENCES [proveedor] ([id_proveedor])
-GO
-
-ALTER TABLE [orden_compra_item] ADD FOREIGN KEY ([orden_id]) REFERENCES [orden_compra] ([id_orden])
-GO
-
-ALTER TABLE [inventario] ADD FOREIGN KEY ([categoria_id]) REFERENCES [inventario_categoria] ([id_categoria])
-GO
-
-ALTER TABLE [orden_compra_item] ADD FOREIGN KEY ([item_tipo]) REFERENCES [inventario] ([id_inventario])
-GO
-
-ALTER TABLE [membresia] ADD FOREIGN KEY ([usuario_id]) REFERENCES [usuario] ([id_usuario])
-GO
-
-ALTER TABLE [orden_compra] ADD FOREIGN KEY ([usuario_id]) REFERENCES [usuario] ([id_usuario])
-GO
-
-ALTER TABLE [clase] ADD FOREIGN KEY ([usuario_id]) REFERENCES [usuario] ([id_usuario])
-GO
-
-ALTER TABLE [clase_inscripcion] ADD FOREIGN KEY ([socio_id]) REFERENCES [socio] ([id_socio])
-GO
-
-ALTER TABLE [clase_inscripcion] ADD FOREIGN KEY ([clase_id]) REFERENCES [clase] ([id_clase])
-GO
-
-ALTER TABLE [inventario_movimiento] ADD FOREIGN KEY ([inventario_id]) REFERENCES [inventario] ([id_inventario])
-GO
-
-ALTER TABLE [inventario_movimiento] ADD FOREIGN KEY ([compra_id]) REFERENCES [orden_compra] ([id_orden])
-GO
-
-ALTER TABLE [dias] ADD FOREIGN KEY ([id_dias]) REFERENCES [clase_dia] ([dias_id])
-GO
-
-ALTER TABLE [clase] ADD FOREIGN KEY ([id_clase]) REFERENCES [clase_dia] ([clase_id])
-GO
