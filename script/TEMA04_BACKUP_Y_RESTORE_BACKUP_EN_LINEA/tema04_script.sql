60479f312136ce02e8a08e17588d044ef371baea
Nahim Martinez <nahimmartinez03@gmail.com>
2025-10-30 12:01:58 -0300

    fix: resolvi las tareas correspondientes al tema 4 BACKUP

--- script/TEMA04_BACKUP_Y_RESTORE_BACKUP_EN_LINEA/tema04_script.sql
+++ script/TEMA04_BACKUP_Y_RESTORE_BACKUP_EN_LINEA/tema04_script.sql
@@ -3,11 +3,8 @@ USE gimnasio_db
 ----------------------------------------------
 --	TEMA: BACKUP Y RESTORE. BACKUP EN LINEA
 ----------------------------------------------
-SELECT * FROM persona;
-SELECT * FROM socio;
-
 --  TAREAS
-------------------------------------------------------------------------------------------------------------------------
+
 --1) Verificar que el modelo de recuperación de la base de datos esté en el modo adecuado para realizar backup en línea
 SELECT name, recovery_model_desc
 FROM sys.databases
@@ -23,6 +20,7 @@ SET RECOVERY FULL;
 -- en el modo correcto.
 
 ------------------------------------------------------------------------------------------------------------------------
+
 --2) Realizar un backup full de la base de datos.
 BACKUP DATABASE gimnasio_db
 TO DISK = 'C:\BackupsSQL\gimnasio_db_full.bak'
@@ -33,7 +31,7 @@ WITH NAME = 'Backup full gimnasio_db'
 ------------------------------------------------------------------------------------------------------------------------
 
 --3) Generar 10 inserts sobre una tabla de referencia.
--- Insertamos 10 nuevas personas
+-- Inserto 10 nuevas personas
 INSERT INTO persona (nombre, apellido, dni, telefono, email) VALUES
 ('Juan', 'Perez', 30123456, 111111, 'juan.perez@email.com'),
 ('Ana', 'Gomez', 31123457, 222222, 'ana.gomez@email.com'),
@@ -46,7 +44,7 @@ INSERT INTO persona (nombre, apellido, dni, telefono, email) VALUES
 ('Diego', 'Diaz', 38123454, 999999, 'diego.diaz@email.com'),
 ('Valeria', 'Moreno', 39123455, 101010, 'valeria.moreno@email.com');
 
--- Como id_socio es clave foránea de persona, insertamos los socios correspondientes.
+-- Como id_socio es clave foránea de persona, inserto los socios correspondientes.
 INSERT INTO socio (id_socio, contacto_emergencia, observaciones) VALUES
 (76, 123456789, 'Rotura de ligamentos'),
 (77, 123456789, ' '),
@@ -59,10 +57,121 @@ INSERT INTO socio (id_socio, contacto_emergencia, observaciones) VALUES
 (84, 123456789, ' '),
 (85, 123456789, ' ');
 
+------------------------------------------------------------------------------------------------------------------------
+
 --4) Realizar backup del archivo de log y registrar la hora del backup
+BACKUP LOG gimnasio_db
+TO DISK = 'C:\BackupsSQL\gimnasio_db_log1.trn'
+WITH NAME = 'Backup del log 1 - Luego de 10 inserts';
+--GUARDO LA HORA:
+SELECT GETDATE() AS HoraBackupLog1;
+
+------------------------------------------------------------------------------------------------------------------------
+
 --5) Generar otros 10 insert sobre la tabla de referencia.
---6) Realizar nuevamente backup de archivo de log  en otro archivo físico.
+-- Inserto otras 10 personas
+INSERT INTO persona (nombre, apellido, dni, telefono, email) VALUES
+('Jorge', 'Ramirez', 40123456, 111112, 'jorge.ramirez@email.com'),
+('Lucia', 'Acosta', 41123457, 222223, 'lucia.acosta@email.com'),
+('Martin', 'Garcia', 42123458, 333334, 'martin.garcia@email.com'),
+('Camila', 'Alvarez', 43123459, 444445, 'camila.alvarez@email.com'),
+('Nicolas', 'Torres', 44123450, 555556, 'nicolas.torres@email.com'),
+('Julieta', 'Ruiz', 45123451, 666667, 'julieta.ruiz@email.com'),
+('Matias', 'Gimenez', 46123452, 777778, 'matias.gimenez@email.com'),
+('Paula', 'Sosa', 47123453, 888889, 'paula.sosa@email.com'),
+('Federico', 'Benitez', 48123454, 999990, 'federico.benitez@email.com'),
+('Agustina', 'Pereyra', 49123455, 101011, 'agustina.pereyra@email.com');
+
+-- Inserto los 10 socios correspondientes
+INSERT INTO socio (id_socio, contacto_emergencia, observaciones) VALUES
+(86, 987654321, 'Lesion femoral'),
+(87, 987654321, ' '),
+(88, 987654321, ' '),
+(89, 987654321, ' '),
+(90, 987654321, 'Entrenamiento liviano'),
+(91, 987654321, ' '),
+(92, 987654321, ' '),
+(93, 987654321, ' '),
+(94, 987654321, ' '),
+(95, 987654321, 'Entrenamiento personalizado');
+
+------------------------------------------------------------------------------------------------------------------------
+
+--6) Realizar nuevamente backup de archivo de log en otro archivo físico.
+BACKUP LOG gimnasio_db
+TO DISK = 'C:\BackupsSQL\gimnasio_db_log2.trn'
+WITH NAME = 'Backup del log 2 - Luego de 10 inserts';
+--GUARDO LA HORA:
+SELECT GETDATE() AS HoraBackupLog2;
+
+------------------------------------------------------------------------------------------------------------------------
+
 --7) Restaurar la base de datos al momento del primer backup del archivo de log. Es decir después de los primeros 10 insert.
+USE master; --CAMBIO DE BASE PARA EVITAR ERRORES DE BASE EN USO
+
+-- Primero debo restaurar el backup completo 
+-- uso NORECOVERY para dejar la base de datos en modo "restauración",
+-- lista para recibir el archivo de log.
+RESTORE DATABASE gimnasio_db
+FROM DISK = 'C:\BackupsSQL\gimnasio_db_full.bak' 
+WITH NORECOVERY, REPLACE;
+
+-- Segundo, Ahora si, una vez estando el modo correcto aplico el primer archivo de log
+-- uso RECOVERY para finalizar el proceso y dejar la base de datos
+-- en línea y lista para usar.
+RESTORE LOG gimnasio_db
+FROM DISK = 'C:\BackupsSQL\gimnasio_db_log1.trn'
+WITH RECOVERY;
+
+------------------------------------------------------------------------------------------------------------------------
+
 --8) Verificar el resultado.
+USE gimnasio_db; --Vuelvo a nuestra base
+
+-- Hago una consulta para contar cuántos socios hay.
+SELECT * FROM socio;
+-- Al hacer esta consulta, en la salida se puede ver que el ultimo o mas alto id que devuelve es el 85,
+-- mientras que en el segundo insert que hice, ingrese socios con id del 86 al 95, los cuales no salen.
+-- esto quiere decir que se realizo correctamente la restauracion
+
+------------------------------------------------------------------------------------------------------------------------
+
 --9) Restaurar la base de datos aplicando ambos archivos de log.
+-- Según lo que yo entendi, el objetivo de este punto es restaurar la base a la ultima version que tengo
+-- donde hice el segundo insert.
+USE master; --CAMBIO DE BASE PARA EVITAR ERRORES DE BASE EN USO
+
+-- De nuevo, empiezo con el backup completo. Lo dejo en modo NORECOVERY
+-- porque voy a aplicarle mas de un archivo de log.
+RESTORE DATABASE gimnasio_db
+FROM DISK = 'C:\BackupsSQL\gimnasio_db_full.bak'
+WITH NORECOVERY, REPLACE;
+
+-- Aplico el primer log, también con NORECOVERY,
+-- porque todavía falta aplicar el segundo log.
+RESTORE LOG gimnasio_db
+FROM DISK = 'C:\BackupsSQL\gimnasio_db_log1.trn'
+WITH NORECOVERY;
+
+-- Ahora aplico el último log. Como este es el final
+-- del proceso, uso RECOVERY para que la base de datos quede en línea y funcional.
+RESTORE LOG gimnasio_db
+FROM DISK = 'C:\BackupsSQL\gimnasio_db_log2.trn'
+WITH RECOVERY;
+
+------------------------------------------------------------------------------------------------------------------------
+
 --10) Expresar sus conclusiones.
+-- Al finalizar este trabajo, pude comprender de mejor manera las tecnicas de backup y restore en SQL SERVER.
+
+-- En el primer ejercicio, entendi que sin establecer la base de datos al modo correcto (FULL), no se puede
+-- implementar una tecnica de backup en linea, ya que en modo simple, no se registran las transacciones en el log
+-- impidiendo la restauracion a un punto especifico en el tiempo.
+
+-- En el proceso de restauracion, entendi que hay que seguir una serie de pasos, que no se pueden romper
+-- siempre partiendo de un backup FULL como base, a partir de alli, se aplican los backups de logs.
+
+-- Al resolver los ejercicios 7) y 9), restaurando la base con uno y dos archivos log, entendi lo potente
+-- que que puede ser esta herramienta, permitiendonos volver atras sin perder lo ultimo que tenemos
+
+------------------------------------------------------------------------------------------------------------------------
