2bda4451e8dd4c748c0b747f8a79434c77490f92
aka-titesen <zgf.nicolas@gmail.com>
2025-11-03 22:50:03 -0300

    fix: Agregear la forma tradicional de agregar personas al Script de insertar personas

--- script/TEMA01_PROCEDIMIENTOS_Y_FUNCIONES_ALMACENADOS/LOTE_USANDO_PROCEDIMIENTO_sp_insertarPersona.sql
+++ script/TEMA01_PROCEDIMIENTOS_Y_FUNCIONES_ALMACENADOS/LOTE_USANDO_PROCEDIMIENTO_sp_insertarPersona.sql
@@ -1,3 +1,24 @@
+/*
+Insertando datos de la forma tradicional
+*/
+
+INSERT INTO persona VALUES ('Carlos', 'Martin', 38211456, 3794112233, 'carlos.martin@mail.com', '2025-09-13', 1);
+INSERT INTO persona VALUES ('Nahim', 'Martinez', 40322344, 3794566999, 'nahim.martinez@mail.com', '2025-09-13', 1);
+INSERT INTO persona VALUES ('Maria Jose', 'Ribas', 39245123, 3794556677, 'Ribas@mail.com', '2025-09-13', 1);
+INSERT INTO persona VALUES ('Juan', 'Mayorana', 40122345, 3794887799, 'juan.mayorana@mail.com', '2025-09-20', 1);
+INSERT INTO persona VALUES ('Pablo', 'Maidana', 38455678, 3813813822, 'pablo.maidana@mail.com', '2025-09-22', 1);
+INSERT INTO persona VALUES ('Juan', 'Camurthers', 39543112, 3132121222, 'juan.camurthers@mail.com', '2025-09-22', 1);
+INSERT INTO persona VALUES ('Lucas', 'Ramírez', 40321123, 3794123001, 'lucas.ramirez@mail.com', '2025-09-14', 1);
+INSERT INTO persona VALUES ('María', 'Gómez', 38456789, 3794123002, 'maria.gomez@mail.com', '2025-09-15', 1);
+INSERT INTO persona VALUES ('Julián', 'Fernández', 39234567, 3794123003, 'julian.fernandez@mail.com', '2025-09-16', 0);
+INSERT INTO persona VALUES ('Camila', 'Ortiz', 40111222, 3794123004, 'camila.ortiz@mail.com', '2025-09-17', 1);
+INSERT INTO persona VALUES ('Tomás', 'López', 38765432, 3794123005, 'tomas.lopez@mail.com', '2025-09-18', 1);
+INSERT INTO persona VALUES ('Sofía', 'Benítez', 39987654, 3794123006, 'sofia.benitez@mail.com', '2025-09-19', 1);
+
+/*
+Insertar lote de datos usando Procedimiento creado previamente
+*/
+
 USE gimnasio_db;
 GO
 
